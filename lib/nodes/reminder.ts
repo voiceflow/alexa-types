97c45ac0af4b626d3e9a2509fcb42b34c1a72de0
Vladimir Profile on Mac <v.yancharuk@gmail.com>
2020-09-07 20:13:32 +0300

    fix: type definitions

--- lib/nodes/reminder.ts
+++ lib/nodes/reminder.ts
@@ -3,21 +3,26 @@ import { DefaultNode, DefaultStep, NodeType } from './types';
 export enum ReminderType {
   SCHEDULED_ABSOLUTE = 'SCHEDULED_ABSOLUTE',
   SCHEDULED_RELATIVE = 'SCHEDULED_RELATIVE',
+}
+
+export enum ReminderClientType {
   TIME = 'timer',
   SCHEDULED = 'scheduled',
 }
 
-export type StepData = {
+export type StepData<T> = {
   reminder: {
     name: string;
-    reminderType: ReminderType;
+    type: T;
     text: string;
-    hours: string;
-    minutes: string;
-    seconds: string;
+    time: {
+      h: string;
+      m: string;
+      s: string;
+    };
     date: string;
     timezone: string;
-    recurrence?: { byDay: string; freq: string };
+    recurrence?: { byDay?: string; freq: string };
     recurrenceBool: boolean;
   };
 };
@@ -25,7 +30,7 @@ export type StepData = {
 export type NodeData = {
   fail_id?: string;
   success_id?: string;
-} & StepData;
+} & StepData<ReminderType>;
 
-export type Step = DefaultStep<NodeType.REMINDER, StepData>;
+export type Step = DefaultStep<NodeType.REMINDER, StepData<ReminderType>>;
 export type Node = DefaultNode<NodeType.REMINDER, NodeData>;
